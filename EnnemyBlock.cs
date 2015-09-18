548fde76d2e78b674bf9723bf2779705e4835de2
unknown <juliensculteur@gmail.com>
2015-09-18 16:12:23 +0200

    Ajout de tous les fichiers

---
+++ EnnemyBlock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SpaceInvaders
+{
+	public class EnnemyBlock
+	{
+		List<SpaceShip> Ships;
+
+		//public Vecteur2D position;
+		public Size size;
+
+		private Vecteur2D speedX;
+		private Vecteur2D speedY;
+
+		public bool alive
+        {
+            private set;
+            get;
+        }
+
+		/*public EnnemyBlock (Vecteur2D position)
+		{
+			this.position = position;
+		}*/
+
+		public void AddLine(int width, int nbShips, int lives, Bitmap im)
+		{
+			
+		}
+
+		public void Draw(Graphics g)
+		{
+			
+		}
+
+		public void Move(double deltaT)
+		{
+			
+		}
+	}
+}
---
+++ Space_invaders/EnemyBlock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvaders
+{
+    class EnemyBlock
+    {
+        public List<SpaceShip> ships;
+        public Vecteur2D position;
+        public Size size;
+        private Vecteur2D speedX;
+        private Vecteur2D speedY;
+        public bool alive
+        {
+            private set;
+            get;
+        }
+
+       public EnemyBlock(Vecteur2D position)
+        {
+
+        }
+
+    }
+}
